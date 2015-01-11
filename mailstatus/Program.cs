1668e6c34cefb114d1c495ac350e34d8e639af3e
moritzuehling <github@moritzuehling.de>
2015-01-12 00:22:23 +0100

    Implemented mailstatus, that shows me the status of my reddit mail.
    If you really are desperate and want to use this: Include it with a
    1-minute refresh period. Add a file cookie.txt to the binary dir, and
    write your password in the first line. Your username has to be changed in
    code. This is because the password solution sucks and I don't want anyone
    to use this. Be careful when commiting your project. You don't want to
    commit your password.

---
+++ mailstatus/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using RedditSharp;
+using System.Reflection;
+
+namespace mailstatus
+{
+	class Program
+	{
+		public static void Main (string[] args)
+		{
+			Directory.SetCurrentDirectory (Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+			var cookie = File.ReadAllLines ("cookie.txt");
+
+			Reddit r = new Reddit ();
+			var me = r.LogIn ("fs0i", cookie [0]);
+
+
+			var m = 0;
+
+			foreach (var messsage in me.UnreadMessages) {
+				m++;
+			}
+
+			string result = "";
+
+			if (m == 0)
+				result += "✉ ";
+			else
+				result += "📩 ";
+
+			if (m != 0)
+				result += "(" + m + ")";
+
+			Console.WriteLine (result);
+			Console.WriteLine (result);
+
+			if (m == 0)
+				Console.WriteLine ("");
+			else if (m < 4)
+				Console.WriteLine ("00ff00");
+			else 
+				Console.WriteLine ("ff0000");
+		}
+	}
+}
+
